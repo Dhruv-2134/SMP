48801be4413e224f469e29c84b83c32d24e933a5
sohail60 <sohailatwork10@gmail.com>
2022-07-31 10:54:45 +0530

    Updated By Sohail

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Login/>
-      <Signup/>
-      <Footer/>
+      {/* <Navbar/> */}
+      {/* <Login/> */}
+      {/* <Signup/> */}
+      <Form/>
+      {/* <Footer/> */}
     </div>
   );
 }
