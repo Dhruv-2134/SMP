98f0333c2171a4dd1995d6c73a7b45bc615ac9c3
shivam <shivprishi@gmail.com>
2022-08-18 18:13:16 +0530

    css file changed to react module

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import '../Private/css/Login.css';
+import styles from '../Private/css/Login.module.css';
 
 
 function Login() {
@@ -14,7 +14,7 @@ function Login() {
 
   if(openForgotPassword){
     window.onclick = function(event) {
-      if (event.target == loginModal.current) {
+      if (event.target === loginModal.current) {
         setOpenForgotPassword(false);
       }
     }
@@ -29,67 +29,68 @@ function Login() {
   }
 
   return (
-    <div className="login-root">
-      <div className="login-body login-page page-hero">
+    <div className={ `${styles.login_root}` }>
+      <div className={ `${styles.login_body} ${styles.login_page} ${styles.page_hero}` }>
         {openForgotPassword && (
-          <div className='login-modal' ref={loginModal}>
-            <div className='login-modal-content'>
+          <div className={styles.login_modal} ref={loginModal}>
+            <div className={styles.login_modal_content}>
                 <button onClick={() => (setOpenForgotPassword(false))} style={{float: "right",color: "#fff",background: "red",border: "none"}}>Close</button>
-                <p style={{fontSize: "3rem",margin: "2rem",color: "#2155CD"}}>Forgot Password</p>
+                <p style={{margin: "2rem",color: "#2155CD"}}>Forgot Password</p>
                   <label>Email</label>
                   <input type={email} placeholder="Enter your email" required/>
-                  <button className="login-modal-submit-button" onClick={submitForgotPassword} >GET OTP</button>
+                  <button className={styles.login_modal_submit_button} onClick={submitForgotPassword} >GET OTP</button>
             </div>
           </div>
-        )}
+        )
+        }
 
-        <div className="prewiev-hero">
+        <div className={styles.prewiev_hero}>
 
           <img src="https://iiitbh.ac.in/sites/default/files/2020-05/Proposed%20IIIT%20Bhagalpur%20Building1.jpg" />
         </div>
-        <div className="login-form form-hero">
-        <h4 className="login-title"  >Indian Institute of Information Technology, Bhagalpur</h4>
-          <div className="form-head">
+        <div className={`${styles.login_form}  ${styles.form_hero}`}>
+          <h4 className={styles.login_title} style={{color: 'darkblue'}} >Indian Institute of Information Technology, Bhagalpur</h4>
+          <div className={styles.form_head}>
         
-            <img src="https://upload.wikimedia.org/wikipedia/en/c/c6/Indian_Institute_of_Information_Technology%2C_Bhagalpur_logo.png" className="login-logo" />
+            <img src="https://upload.wikimedia.org/wikipedia/en/c/c6/Indian_Institute_of_Information_Technology%2C_Bhagalpur_logo.png" className={styles.login_logo} />
             
-            <h6 className="login-title login-portal-text">Student Portal</h6>
+            <h6 className={`${styles.login_title} ${styles.login_portal_text}`}>Student Portal</h6>
           </div>
-          <form onSubmit={()=> {}} className="form">
-            <label className="input-hero password-input" htmlFor="userName">
-                <input value={userName} onChange={(event)=> (setUserName(event.target.value))} type="UserName" name="userName" id="userName" required />
-                <span className="input-name"> User Name </span>
-                <span className="input-icon">
+          <form onSubmit={()=> {}} className={styles.form}>
+            <label className={`${styles.input_hero} ${styles.password_input}`} htmlFor="userName">
+                <input value={userName} onChange={(event)=> (setUserName(event.target.value))} type="text" name="userName" id="userName" required />
+                <span className={styles.input_name}> User Name </span>
+                <span className={styles.input_icon}>
                   </span>
 
             </label>
-            <label className="input-hero password-input" htmlFor="email">
+            <label className={`${styles.input_hero} ${styles.password_input}`} htmlFor="email">
               <input value={email} onChange={(event) => (setEmail(event.target.value))} type="email" name="email" id="email" required />
-              <span className="input-name"> Email </span>
-              <span className="input-icon">
+              <span className={styles.input_name}> Email </span>
+              <span className={styles.input_icon}>
                 </span>
             
             </label>
 
-            <label className="input-hero password-input" htmlFor="password">
+            <label className={`${styles.input_hero} ${styles.password_input}`} htmlFor="password">
               <input value={password} onChange={(event) => (setPassword(event.target.value))} type="password" name="password" id="password" required />
-              <span className="input-name"> Password </span>
-              <span className="input-icon">
+              <span className={styles.input_name}> Password </span>
+              <span className={styles.input_icon}>
                 </span>
-              <a onClick={() => (setOpenForgotPassword(true))} href="#" className="recovery-password"> Recovery password </a>
+              <a onClick={() => (setOpenForgotPassword(true))} href="#" className={styles.recovery_password}> Recovery password </a>
             </label>
 
-            <label htmlFor="remember" className="checkbox-hero">
+            <label htmlFor="remember" className={styles.checkbox_hero}>
               <input onClick={()=> setRemeberMe((preValue) => (!preValue))} type="checkbox" name="remember" id="remember" />
 
-              <span className="checkbox-cover"></span>
-              <span className="checkbox-name"> Remember Me </span>
+              <span className={styles.checkbox_cover}></span>
+              <span className={styles.checkbox_name}> Remember Me </span>
             </label>
 
 
-            <button onClick={submitLogin} type="submit" className="submit-btn btn">Login</button>
+            <button onClick={submitLogin} type="submit" className={`${styles.submit_btn} ${styles.btn}`}>Login</button>
 
-            <div className="form-bottom">
+            <div className={styles.form_bottom}>
               Don't have an account ?
               <a href="#">Sign Up</a>
             </div>
